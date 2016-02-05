234c07fe8fc9fd5c5fa725c93cc5da3919db9d51
Bernardo Pires <carneiro.be@gmail.com>
2016-02-05 12:20:19 +0100

    Added flask server for the elevator

---
+++ flask_server.py
@@ -0,0 +1,56 @@
+import os
+import time
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+# Todo: log everything
+LOCK_FILENAME = "reserved_for.txt"
+LOCK_TIMER_SECONDS = 30
+
+def get_status_and_maybe_release_lock():
+    if os.path.isfile(LOCK_FILENAME):
+        # Release lock if needed
+        file_age_in_seconds = time.time() - os.path.getmtime(LOCK_FILENAME)
+        if file_age_in_seconds > LOCK_TIMER_SECONDS:
+            os.remove(LOCK_FILENAME)
+            return "FREE"
+        else:
+            with open(LOCK_FILENAME) as f:
+                return f.read()
+    else:
+        return "FREE"
+
+def reserve_for(ip):
+    with open(LOCK_FILENAME, 'w') as f:
+        f.write(ip)
+
+@app.route("/status/")
+def status():
+    # Returns either FREE or BUSY.
+    return "BUSY" if get_status_and_maybe_release_lock() != "FREE" else "FREE"
+
+@app.route("/go_up/")
+def go_up():
+    # Reserves the elevator (lock). Returns either BUSY or OK.
+    if get_status_and_maybe_release_lock() == "FREE":
+        reserve_for(request.remote_addr)
+        # TODO: insert magic call to tell the elevator to go up
+        return "OK"
+    else:
+        return "BUSY"
+
+@app.route("/go_down/")
+def go_down():
+    # Only the robot that reserved it can call it!
+    print get_status_and_maybe_release_lock()
+    if get_status_and_maybe_release_lock() != request.remote_addr:
+        return "BAD, BAD ROBOT!"
+
+    # Renew locker time
+    os.utime(LOCK_FILENAME, None)
+    # TODO: insert magic call to tell the elevator to go down
+    return "OK"
+
+if __name__ == "__main__":
+    app.run(debug=True)#host="192.168.0.135")
