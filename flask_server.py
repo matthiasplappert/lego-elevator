7d0bbd9a30f02564b9be22a55e637e9de9f05413
Matthias Plappert <matthiasplappert@me.com>
2016-02-09 13:32:12 +0100

    Add watchdog thread that clears up the lock

--- flask_server.py
+++ flask_server.py
@@ -13,37 +13,45 @@ app = Flask(__name__)
 LOCK_FILENAME = "reserved_for.txt"
 LOCK_TIMER_SECONDS = 60
 
+# Elevator thread
 kill_event = threading.Event()
 queue = Queue()
 elevator = Elevator(kill_event)
 elevator.daemon = True
 elevator.start()
 
+# Watch dog thread
+def watch_dog():
+    while True:
+        # Clean up if lock has expired.
+        if os.path.isfile(LOCK_FILENAME):
+            file_age_in_seconds = time.time() - os.path.getmtime(LOCK_FILENAME)
+            if file_age_in_seconds > LOCK_TIMER_SECONDS:
+                os.remove(LOCK_FILENAME)
+                if elevator.status not in ("DOWN", "FREE"):
+                    elevator.onThread(elevator.down)
+        time.sleep(1)
+watch_dog_thread = threading.Thread(target=watch_dog)
+watch_dog_thread.daemon = True
+watch_dog_thread.start()
+
 start_time = datetime.now() + timedelta(minutes=5)
 
-def get_status_and_maybe_release_lock():
+
+def get_status():
     if os.path.isfile(LOCK_FILENAME):
-        # Release lock if needed
-        file_age_in_seconds = time.time() - os.path.getmtime(LOCK_FILENAME)
-        if file_age_in_seconds > LOCK_TIMER_SECONDS:
-            os.remove(LOCK_FILENAME)
-            if elevator.status not in ("DOWN", "GOING_DOWN", "FREE"):
-                elevator.onThread(elevator.down)
-                return "BUSY"
-            return "FREE"
-        else:
-            return "BUSY"
+        # The elevator is reserved, hence the status is busy.
+        return "BUSY"
+    elif elevator.status not in ("DOWN", "FREE"):
+        # The elevator is currently not DOWN or FREE, hence busy.
+        return "BUSY"
     else:
+        # The elevator is free.
         return "FREE"
 
 
-def is_reserved_for_ip(ip):
+def is_reserved_for(ip):
     if not os.path.isfile(LOCK_FILENAME):
-        return False
-
-    file_age_in_seconds = time.time() - os.path.getmtime(LOCK_FILENAME)
-    if file_age_in_seconds > LOCK_TIMER_SECONDS:
-        os.remove(LOCK_FILENAME)
         return False
 
     locked_ip = None
@@ -59,7 +67,7 @@ def reserve_for(ip):
 @app.route("/status/")
 def status():
     # Returns either FREE or BUSY.
-    return "BUSY" if get_status_and_maybe_release_lock() != "FREE" or \
+    return "BUSY" if get_status() != "FREE" or \
             elevator.status != "FREE" else "FREE"
 
 @app.route("/elevator_status/")
@@ -69,7 +77,7 @@ def elevator_status():
 @app.route("/go_up/")
 def go_up():
     # Reserves the elevator (lock). Returns either BUSY or OK.
-    if get_status_and_maybe_release_lock() == "FREE":
+    if get_status() == "FREE":
         reserve_for(request.remote_addr)
         elevator.onThread(elevator.up)
         return "OK"
@@ -80,7 +88,7 @@ def go_up():
 def go_down():
     # Only the robot that reserved it can call it!
     # todo: only allow the robot to call this once?
-    if not is_reserved_for_ip(request.remote_addr):
+    if not is_reserved_for(request.remote_addr):
         return "BAD, BAD ROBOT!"
 
     if elevator.status != "UP":
